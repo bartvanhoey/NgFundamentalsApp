b800c2ed0d4cd2163834ee5067f976faee9dff79
bartvanhoey <bartvanhoey@hotmail.com>
2019-07-22 17:12:34 +0200

    end part a brief look at the app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
-  declarations: [
-    EventsAppComponent
-  ],
   imports: [
     BrowserModule
+  ],  declarations: [
+    EventsAppComponent
   ],
-  providers: [],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
