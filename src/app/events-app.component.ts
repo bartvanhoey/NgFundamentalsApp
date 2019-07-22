2c56627ee49b7f0470fdcd974f0ef5b9161c025d
bartvanhoey <bartvanhoey@hotmail.com>
2019-07-22 17:08:40 +0200

    first commit

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-app-component',
+  template: '<h2>hello world</h2>'
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals-app';
+}
