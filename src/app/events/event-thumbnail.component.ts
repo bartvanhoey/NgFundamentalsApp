5448dc74dcb8606a246e7576a917ac8edc1a09bd
bartvanhoey <bartvanhoey@hotmail.com>
2019-07-23 19:54:18 +0200

    end part-8

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,20 +2,14 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
     selector: 'app-event-thumbnail',
-    templateUrl: './event-thumbnail.component.html'
+    templateUrl: './event-thumbnail.component.html',
+    styles: [`
+      .pad-left { margin-left: 10px;}
+      .well div { color: #bbb; }
+    `]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
   @Output() eventClick = new EventEmitter();
-  someProperty = 'somePropertyValue';
-
-  handleClickMe() {
-    this.eventClick.emit(this.event.name);
-  }
-
-  logFoo() {
-    console.log('foo!');
-  }
-
 }
 
