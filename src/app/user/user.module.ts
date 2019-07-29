55c5709701799ec06c43d39e7edcc66c5cf42609
bartvanhoey <bartvanhoey@hotmail.com>
2019-07-29 11:34:07 +0200

    end part 32

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,14 +3,18 @@ import { CommonModule} from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 import { userRoutes } from './user.routes';
+import { LoginComponent } from './login.components';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   providers: [
   ]
