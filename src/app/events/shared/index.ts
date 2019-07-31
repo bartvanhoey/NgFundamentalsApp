ca23ff2583b1523a239ebc44af10318119d9afed
bartvanhoey <bartvanhoey@hotmail.com>
2019-07-31 21:28:33 +0200

    end part 46

--- src/app/events/shared/index.ts
+++ src/app/events/shared/index.ts
@@ -2,3 +2,7 @@ export * from './event.service';
 export * from './event.model';
 export * from './session.model';
 export * from './restricted-words.validator';
+export * from './restricted-words.validator';
+export * from './duration.pipe';
+
+
