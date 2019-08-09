19da9f6b98ee2f2929179512b257c82370910e18
bartvanhoey <bartvanhoey@hotmail.com>
2019-08-09 05:56:44 +0200

    end part 51

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -274,9 +274,10 @@ const EVENTS: IEvent[] = [{
   }]
 }];
 
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
 import { IEvent } from './event.model';
+import { ISession } from '.';
 
 
 @Injectable()
@@ -296,9 +297,33 @@ export class EventService {
   getEvent(id: number): IEvent {
     return EVENTS.find(event => event.id === id);
   }
+
   updateEvent(event: IEvent) {
     const index = EVENTS.findIndex(e => e.id === event.id);
     EVENTS[index] = event;
+  }
+
+  searchSessions(searchTerm: string) {
+    const term = searchTerm.toLocaleLowerCase();
+    let results: ISession[] = [];
+
+    EVENTS.forEach(event => {
+      let matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
+
+      matchingSessions = matchingSessions.map((session: any) => {
+        session.eventId = event.id;
+        return session;
+      });
+      results = results.concat(matchingSessions);
+    });
+
+    const emitter = new EventEmitter(true);
+
+    setTimeout(() => {
+      emitter.emit(results);
+    }, 1000);
+
+    return emitter;
   }
 }
 
