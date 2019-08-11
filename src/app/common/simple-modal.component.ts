0a1961d84827aef1e819514f35803e1dab6021dc
bartvanhoey <bartvanhoey@hotmail.com>
2019-08-11 15:43:46 +0200

    end part 57

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+import { JQ_TOKEN } from './jQuery.service';
 @Component({
     selector: 'app-simple-modal',
     templateUrl: './simple-modal.component.html',
@@ -10,4 +10,11 @@ import { Component, Input } from '@angular/core';
 export class SimpleModalComponent {
     @Input() title: string;
     @Input() elementId: string;
+    @ViewChild('modalcontainer', { static: false }) containerEl: ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $: any) {}
+
+    closeModal() {
+        this.$(this.containerEl.nativeElement).modal('hide');
+    }
 }
